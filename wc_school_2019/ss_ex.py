c0de0072ce4460b860247fb8622623b69ab111a5
Arthur Goldberg <artgoldberg@gmail.com>
2019-04-04 20:22:05 -0400

    add trajectory_exercise

--- wc_school_2019/ss_ex.py
+++ wc_school_2019/ss_ex.py
@@ -1,4 +1,4 @@
-from mrna_and_proteins_using_several_methods import deterministic_exercise
+from mrna_and_proteins_using_several_methods import deterministic_exercise, trajectory_exercise
 
 deterministic_exercise()
 

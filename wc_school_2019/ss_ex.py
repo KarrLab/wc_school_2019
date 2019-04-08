9aa418ee70a57c52430417e3b48d99649de3780c
Arthur Goldberg <artgoldberg@gmail.com>
2019-04-08 17:00:10 +0200

    create and add figures

--- wc_school_2019/ss_ex.py
+++ wc_school_2019/ss_ex.py
@@ -1,5 +1,8 @@
-from mrna_and_proteins_using_several_methods import deterministic_exercise, trajectory_exercise
+from mrna_and_proteins_using_several_methods import (deterministic_exercise, trajectory_exercise,
+    probability_distribution_exercise)
 
 deterministic_exercise()
 
 trajectory_exercise()
+
+probability_distribution_exercise()
